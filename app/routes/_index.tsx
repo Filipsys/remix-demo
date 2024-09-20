b93ef08d082733cbeb2272355debe57b641f59d1
filyys <filipsysak@gmail.com>
2024-09-20 23:11:23 +0200

    Added pmndrs drei, started trying out 3d models, still low quality and bad shadows. Working on those, no clue how to fix it yet. 2/3

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/no-unknown-property */
 import type { MetaFunction } from "@remix-run/node";
-import { Canvas } from "@react-three/fiber";
-import { useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls, SoftShadows } from "@react-three/drei";
+import { SmallBagModel } from "~/components/SmallBag";
+import { Model } from "~/components/Comp1";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,18 +14,26 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  const gltf = useLoader(GLTFLoader, "/small_bag.glb");
+  // const { nodes, materials } = useLoader(GLTFLoader, "/small_bag.glb");
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <div id="canvas-container">
+      <div id="canvas-container" className="w-full h-full">
         <Canvas>
           <ambientLight intensity={0.1} />
-          <directionalLight color="red" position={[0, 0, 5]} />
+          <directionalLight position={[0, 0, 1]} intensity={0.6} />
+          <directionalLight position={[0, 0, -5]} intensity={0.6} />
 
-          <mesh>
-            <primitive object={gltf.scene} />
+          <mesh scale={40} position={[0, 0, 0]} rotation={[-1.6, 0.2, 2.5]}>
+            <OrbitControls />
+            
+            <Model />
           </mesh>
+
+          {/* <mesh>
+            <boxGeometry />
+            <meshStandardMaterial color="red" />
+          </mesh> */}
         </Canvas>
       </div>
     </div>
