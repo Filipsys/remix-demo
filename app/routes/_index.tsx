de0b261f018eb247ef3f623eada3813c83db75bd
filyys <filipsysak@gmail.com>
2024-09-21 23:14:28 +0200

    Added 3d money model, hemisphere lighting, fixed low quality model

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,13 @@
 /* eslint-disable react/no-unknown-property */
 import type { MetaFunction } from "@remix-run/node";
 import { Canvas, useLoader } from "@react-three/fiber";
-import { OrbitControls, SoftShadows } from "@react-three/drei";
-import { SmallBagModel } from "~/components/SmallBag";
+import {
+  AsciiRenderer,
+  Billboard,
+  OrbitControls,
+  Outlines,
+  Text,
+} from "@react-three/drei";
 import { Model } from "~/components/Comp1";
 // import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
@@ -18,22 +23,17 @@ export default function Index() {
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <div id="canvas-container" className="w-full h-full">
-        <Canvas>
-          <ambientLight intensity={0.1} />
-          <directionalLight position={[0, 0, 1]} intensity={0.6} />
-          <directionalLight position={[0, 0, -5]} intensity={0.6} />
+      <div id="canvas-container" className="w-full h-full bg-zinc-900">
+        <Canvas frameloop="demand">
+          <ambientLight intensity={0.6} />
+          <hemisphereLight position={[0, 0, 1]} intensity={0.3} />
 
           <mesh scale={40} position={[0, 0, 0]} rotation={[-1.6, 0.2, 2.5]}>
-            <OrbitControls />
-            
-            <Model />
-          </mesh>
+            {/* <OrbitControls /> */}
 
-          {/* <mesh>
-            <boxGeometry />
+            <Model />
             <meshStandardMaterial color="red" />
-          </mesh> */}
+          </mesh>
         </Canvas>
       </div>
     </div>
