6c0dd1ca50ddd6d279018a42c45be4f7ec8e19e6
filyys <filipsysak@gmail.com>
2024-09-21 23:41:13 +0200

    Changed background, text gradient

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -22,13 +22,18 @@ export default function Index() {
   // const { nodes, materials } = useLoader(GLTFLoader, "/small_bag.glb");
 
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div id="canvas-container" className="w-full h-full bg-zinc-900">
+    <div className="flex h-screen items-center justify-center bg-gradient-to-tr from-zinc-950 to-zinc-800">
+      <div className="w-2/3 h-full  flex items-center justify-center">
+        <h1 className="text-center text-[256px] font-bold bg-gradient-to-br from-gray-300 to-gray-600 text-transparent bg-clip-text drop-shadow-xl">
+          Money.
+        </h1>
+      </div>
+      <div id="canvas-container" className="w-1/3 h-full ">
         <Canvas frameloop="demand">
           <ambientLight intensity={0.6} />
           <hemisphereLight position={[0, 0, 1]} intensity={0.3} />
 
-          <mesh scale={40} position={[0, 0, 0]} rotation={[-1.6, 0.2, 2.5]}>
+          <mesh scale={40} position={[-0.6, 0, 1]} rotation={[-1.6, 0.2, 2.5]}>
             {/* <OrbitControls /> */}
 
             <Model />
